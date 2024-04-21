df744133e20c1fe61cc47d39446f44d9c29cd8ad
ercelserhat <ercelserhat@gmail.com>
2024-04-22 01:12:43 +0300

    Add HeaderView to HomePageView

--- RecipesApp/View/HomePageView.swift
+++ RecipesApp/View/HomePageView.swift
@@ -9,7 +9,18 @@ import SwiftUI
 
 struct HomePageView: View {
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        ZStack{
+            LinearGradient(colors: [.clear, .gray.opacity(0.1)], startPoint: .top, endPoint: .bottom)
+                .ignoresSafeArea()
+            NavigationStack{
+                ScrollView{
+                    VStack{
+                        HeaderView()
+                    }
+                    .padding(.horizontal)
+                }
+            }
+        }
     }
 }
 
