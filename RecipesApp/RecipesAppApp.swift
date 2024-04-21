f3d3ee85c24e83586f10401edb6f635ed314eab4
ercelserhat <ercelserhat@gmail.com>
2024-04-21 23:39:56 +0300

    Initial Commit

---
+++ RecipesApp/RecipesAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  RecipesAppApp.swift
+//  RecipesApp
+//
+//  Created by Serhat Erçel on 21.04.2024.
+//
+
+import SwiftUI
+
+@main
+struct RecipesAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
