13e23710ade0fbb61a08a543315c369ed08332dc
ercelserhat <ercelserhat@gmail.com>
2024-04-21 23:48:17 +0300

    Create TabView

--- RecipesApp/ContentView.swift
+++ RecipesApp/ContentView.swift
@@ -8,14 +8,40 @@
 import SwiftUI
 
 struct ContentView: View {
+    @State private var selectedTab: Int = 0
+    
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+        TabView(selection: $selectedTab){
+            Text("Home")
+                .tabItem {
+                    Image(systemName: "house.fill")
+                    Text("Home")
+                }
+                .tag(0)
+            Text("Discover")
+                .tabItem {
+                    Image(systemName: "location.fill")
+                    Text("Discover")
+                }
+                .tag(1)
+            Text("Add")
+                .tabItem {
+                    Image(systemName: "plus.diamond")
+                    Text("Add")
+                }
+                .tag(2)
+            Text("Shopping")
+                .tabItem {
+                    Image(systemName: "cart.fill")
+                    Text("Shop")
+                }
+                .tag(3)
+            Text("Profile")
+                .tabItem {
+                    Image(systemName: "person.fill")
+                    Text("Profile")
+                }
         }
-        .padding()
     }
 }
 
